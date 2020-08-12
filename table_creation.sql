805e766d47d246c58c6b4037436d17007d4b53bd
bescoto98 <escoto.brenda42@gmail.com>
2020-08-12 12:20:48 -0700

    added functionality

--- table_creation.sql
+++ table_creation.sql
@@ -1,12 +1,60 @@
 --user table
-
-create table users(idnum not null, FN varchar(50), LN varchar(50), type int, primary key(idnum));
+create table users(
+  u_id int primary key unique,
+  first_name varchar(50),
+  last_name varchar(50),
+  u_type int);
 
 --account table
-create table accounts(acctid not null, idnum int not null, balance double, primary key (acctid), foreign key(idnum) references user(idnum));
+create table accounts(
+  a_id int primary key unique,
+  u_id int not null,
+  balance money,
+  a_type varchar(50),
+  foreign key(u_id) references users(u_id));
 
 -- login table
-create table logins(idnum not null, username varchar(50), password varchar(50), foreign key (idnum) references user(idnum));
+create table logins(
+  u_id int primary key,
+  username varchar(50) unique,
+  password varchar(50),
+  foreign key(u_id) references users(u_id));
 
 --personal info table
-create table information(idnum not null, ssn int, address varchar(50), city varchar(50), state varchar(25), zip varchar(25), phoneNum varchar(25), email varchar(50), foreign key idnum references user(idnum));
+create table information(
+	u_id int primary key,
+	ssn varchar(9),
+	address varchar(50),
+	city varchar(50),
+	state varchar(25),
+	zip varchar(25),
+	phone varchar(15),
+	email varchar(50),
+	foreign key (u_id) references users(u_id)
+);
+
+--approvals table
+create table approvals(
+  trans_id int primary key unique,
+  a_id int,
+  approvedBy int,
+  is_approved boolean,
+  appr_date date,
+  foreign key (a_id) references accounts(a_id),
+);
+
+
+-- sample values
+insert into users values(49,'steve','orange',4);
+select * from users;
+
+insert into accounts values(999,49,85.78);
+select * from accounts;
+
+select * from accounts left join users on users.idnum = accounts.idnum;
+
+insert into logins values (49,'steve90','password30');
+
+select * from users
+	left join accounts on users.idnum = accounts.idnum
+	left join logins on users.idnum = logins.idnum;
